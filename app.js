de5aeb66e159293b9fe0f4cbfffc7fec7df56424
Chaudhary Tirth <CHAUDHARYTIRTH23@GMAIL.COM>
2025-08-19 16:43:45 +0530

    Update app.js
    swith to another free api

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-let apiKey = "63e85fe008b647c784be9a99e47497a0";
-let page = 1;
-let currentCategory = "general";
-let pageSize = 3;
-let placeholderImg = "my-placeholder.png"; // <-- your custom placeholder image
+let currentCategory = "world"; 
+let placeholderImg = "placeholder.png";
 
-function loadCategory(category, loadMore = false) {
-  if (!loadMore) {
-    page = 1;
-    currentCategory = category;
-    document.getElementById("news-container").innerHTML = ""; // clear old news
-  }
+function loadCategory(category) {
+  currentCategory = category;
+  document.getElementById("news-container").innerHTML = ""; 
 
-  let url = `https://newsapi.org/v2/top-headlines?country=us&category=${currentCategory}&pageSize=${pageSize}&page=${page}&apiKey=${apiKey}`;
-  let placeholderImg=" placeholder.png";
-
+  let url = `https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=us&max=10&apikey=e031a516483241b4208938054460ccf4`;
 
   fetch(url)
     .then(res => res.json())
     .then(data => {
+      console.log(data); 
+
       if (!data.articles || data.articles.length === 0) {
-        alert("No more news available.");
+        alert("No news available.");
         return;
       }
 
@@ -28,7 +22,7 @@ function loadCategory(category, loadMore = false) {
       data.articles.forEach(article => {
         let card = `
           <div class="card">
-            <img src="${article.urlToImage ? article.urlToImage : placeholderImg}" alt="article_image">
+            <img src="${article.image ? article.image : placeholderImg}" alt="article_image">
             <h3>${article.title}</h3>
             <p>${article.description || "No description available."}</p>
             <a href="${article.url}" target="_blank">Read more...</a>
@@ -40,13 +34,7 @@ function loadCategory(category, loadMore = false) {
     .catch(error => console.log("Error fetching news:", error));
 }
 
-// Load next page
-function loadMore() {
-  page++;
-  loadCategory(currentCategory, true);
-}
-
-// Load default category on page load
 window.onload = function() {
-  loadCategory("general");
+  loadCategory("world"); 
 };
+
